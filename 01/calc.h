805c7b16602446b91cbc8564b402cc64478a7604
Alex <54078844+AZelentsov343@users.noreply.github.com>
2019-10-13 20:24:36 +0300

    Add files via upload

---
+++ 01/calc.h
@@ -0,0 +1,7 @@
+#include "list.cpp"
+
+int prior(int c);
+int count(int a, int op, int b);
+int is_aryphmetic(char a);
+void rec_delete(struct List *l);
+int calc(char *s, struct List *prev = nullptr);
