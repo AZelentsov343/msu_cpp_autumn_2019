ba75984a7c1bc3c86605b8e7e7833b0aea18cced
Alex <54078844+AZelentsov343@users.noreply.github.com>
2019-10-15 21:28:21 +0300

    Delete calc.h

--- 01/calc.h
+++
@@ -1,7 +0,0 @@
-#include "list.cpp"
-
-int prior(int c);
-int count(int a, int op, int b);
-int is_aryphmetic(char a);
-void rec_delete(struct List *l);
-int calc(char *s, struct List *prev = nullptr);
