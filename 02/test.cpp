f151389d5a6a2a1b60e3b0e1cfab34b927f5c299
AZelentsov343 <ya.zelentsov@gmail.com>
2019-10-18 11:21:04 +0300

    add files via upload

--- 02/test.cpp
+++ 02/test.cpp
@@ -8,48 +8,52 @@
 //Func main is used only to test LinearAllocator
 int main() {
     using namespace std;
-
-    auto la = LinearAllocator(4);
-    char *ptr1 = la.alloc(2);
-    if (ptr1 == nullptr) {
-        cerr << "Error: allocator cannot allocate memory" << endl;
+    try {
+        auto la = LinearAllocator(4);
+        char *ptr1 = la.alloc(2);
+        if (ptr1 == nullptr) {
+            cerr << "Error: allocator cannot allocate memory" << endl;
+            return 1;
+        }
+        ptr1[0] = 'a';
+        ptr1[1] = '\0';
+        if (strcmp(ptr1, "a") != 0) {
+            cerr << "Error: cannot use allocated memory" << endl;
+            return 1;
+        }
+        char *ptr2 = la.alloc(4);
+        if (ptr2 != nullptr) {
+            cerr << "Error: allocator still allocates memory after overflow" << endl;
+            return 1;
+        }
+        la.reset();
+        char *ptr3 = la.alloc(1);
+        if (ptr1 != ptr3) {
+            cerr << "Error: allocator does not reset correct" << endl;
+            return 1;
+        }
+        la.reset();
+        char *ptr4 = la.alloc(10);
+        if (ptr4 != nullptr) {
+            cerr << "Error: allocator still allocates memory after reset and overflow" << endl;
+            return 1;
+        }
+        Timer t;
+        t.start();
+        char *ptr5 = (char *) malloc(4);
+        long long res1 = t.finish();
+        t.start();
+        char *ptr6 = la.alloc(4);
+        long long res2 = t.finish();
+        if (res2 > res1) {
+            cerr << "Allocator works much slower than it supposed to" << endl;
+            return 1;
+        }
+        cout << "Everything OK" << endl;
+        return 0;
+    } catch (Exception &e) {
+        e.print_message();
         return 1;
     }
-    ptr1[0] = 'a';
-    ptr1[1] = '\0';
-    if (strcmp(ptr1, "a") != 0) {
-        cerr << "Error: cannot use allocated memory" << endl;
-        return 1;
-    }
-    char *ptr2 = la.alloc(4);
-    if (ptr2 != nullptr) {
-        cerr << "Error: allocator still allocates memory after overflow" << endl;
-        return 1;
-    }
-    la.reset();
-    char *ptr3 = la.alloc(1);
-    if (ptr1 != ptr3) {
-        cerr << "Error: allocator does not reset correct" << endl;
-        return 1;
-    }
-    la.reset();
-    char *ptr4 = la.alloc(10);
-    if (ptr4 != nullptr) {
-        cerr << "Error: allocator still allocates memory after reset and overflow" << endl;
-        return 1;
-    }
-    Timer t;
-    t.start();
-    char *ptr5 = (char *)malloc(4);
-    long long res1 = t.finish();
-    t.start();
-    char *ptr6 = la.alloc(4);
-    long long res2 = t.finish();
-    if (res2 > res1) {
-        cerr << "Allocator works much slower than it supposed to" << endl;
-        return 1;
-    }
-    cout << "Everything OK" << endl;
-    return 0;
 }
 
